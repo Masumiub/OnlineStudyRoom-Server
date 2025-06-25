18ce35197c81bfb4770664b64409aecbd4dc551b
Md. Masum Musfique <66321598+Masumiub@users.noreply.github.com>
2025-06-25 11:08:15 +0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# OnlineStudyRoom-Server
-OnlineStudyRoom Server
+# Online Study Room
+This app is about a online group study with friends (Every registered user is a friend of others). Users can create assignments, complete them, and grade their friends' assignments. 
+
+# Live Link:
+https://onlinestudyroom-all.web.app/
+
+# Features
+ - Collaborative Assignment: Registered users can create assignments that their friends can attempt. They Customize topics, instructions, and deadlines to suit their study group’s needs.
+ - Peer-to-Peer Grading: Registered users can review and grade their friends’ submissions with transparency. They can get valuable feedback on their own work while helping others improve.
+ - Track Submissions: Users can view all their attempted assignments, scores, and feedback in one organized dashboard.
+ - Secure Login with Firebase: We use Firebase Authentication to ensure only verified users can access and interact with the platform.
+ - Search & Sort Assignments: Search & sort aasignmnts by level of difficulty.
+ - User Friendly UI with Dark Mood: This app comes with User Friendly UI with Dark Mood.
+
+# NPM Packages:
+- Firebase 
+- Firbase admin SDK
+- axios
+- lottie-react
+- react motion
+- react-awesome-reveal
+- react-countup
+- react-datepicker
+- react-icons
+- react-router
+- react-toastify
+- sweetalert2
+- tailwindcss + daisy UI
+
+
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+## Expanding the ESLint configuration
+
+If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
---
+++ index.js
@@ -0,0 +1,253 @@
+const express = require('express')
+require('dotenv').config()
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const port = process.env.PORT || 3000
+const app = express()
+var admin = require("firebase-admin");
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf-8')
+
+var serviceAccount = JSON.parse(decoded);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+
+
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://onlinestudyroom-all.web.app'],
+    credentials: true,
+}))
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.upsc470.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+const verifyJWT = async (req, res, next) => {
+    const token = req?.headers?.authorization?.split(' ')[1]
+    //console.log(token);
+
+    if (!token) return res.status(401).send({ message: 'Unauthorized Access!' })
+    try {
+        const decoded = await admin.auth().verifyIdToken(token)
+        req.tokenEmail = decoded.email
+        //console.log(decoded)
+        next()
+    } catch (err) {
+        //console.log(err)
+        return res.status(401).send({ message: 'Unauthorized Access!' })
+    }
+}
+
+
+async function run() {
+    try {
+        const database = client.db('OnlineStudyRoomDB')
+        const assignmentCollection = database.collection('assignments')
+        const submissionCollection = database.collection('submissions')
+
+        app.get('/latestAssignments', async (req, res) => {
+            try {
+                const limit = parseInt(req.query.limit) || 0;
+                const sortByPostedAt = req.query.sort === 'postedAt';
+
+                let cursor = assignmentCollection.find({});
+
+                if (sortByPostedAt) {
+                    cursor = cursor.sort({ postedAt: 1 });
+                }
+
+                if (limit > 0) {
+                    cursor = cursor.limit(limit);
+                }
+
+                const latestAssignments = await cursor.toArray();
+                res.send(latestAssignments);
+            } catch (error) {
+                res.status(500).send({ message: 'Failed to fetch tasks', error });
+            }
+        })
+
+        app.get('/assignments', async (req, res) => {
+            const allAssignments = await assignmentCollection.find().toArray();
+            res.send(allAssignments);
+        })
+
+
+        app.post('/create-assignment', verifyJWT, async (req, res) => {
+            const assignmentData = req.body;
+            const result = await assignmentCollection.insertOne(assignmentData);
+            res.send(result);
+        })
+
+        app.put('/update-assignment/:id', verifyJWT, async (req, res) => {
+            try {
+                const id = req.params.id;
+                const query = { _id: new ObjectId(id) };
+                const options = { upsert: true };
+
+                const updatedAssignment = req.body;
+                const updatedDoc = {
+                    $set: updatedAssignment
+                }
+
+                const result = await assignmentCollection.updateOne(query, updatedDoc, options);
+                res.send(result);
+            }
+            catch (error) {
+                console.error(error);
+                res.status(500).json({ message: 'Update Failed', error: error.message });
+            }
+        })
+
+        app.delete('/assignments/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await assignmentCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+        //handle assignment submission
+        app.post('/submissions/:assignmentId', async (req, res) => {
+            const id = req.params.assignmentId;
+            const submissionData = req.body;
+            const result = await submissionCollection.insertOne(submissionData);
+            res.send(result);
+        })
+
+
+        app.get('/assignments/level/:level', async (req, res) => {
+            const level = req.params.level;
+
+            if (level === "All") {
+                const allAssignments = await assignmentCollection.find().toArray();
+                return res.send(allAssignments);
+            }
+
+            const query = { level: level };
+            const filteredAssignments = await assignmentCollection.find(query).toArray();
+            res.send(filteredAssignments);
+
+        })
+
+        app.get('/assignments/search', async (req, res) => {
+            const queryText = req.query.query || "";
+
+            const searchRegex = new RegExp(queryText, "i"); 
+            const query = { title: { $regex: searchRegex } };
+
+            try {
+                const results = await assignmentCollection.find(query).toArray();
+                res.send(results);
+            } catch (error) {
+                res.status(500).json({ message: 'Search failed', error: error.message });
+            }
+        });
+
+
+        //get all assignment submission by a user
+        app.get('/my-attempts/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const tokenEmail = req.tokenEmail;
+
+            if (email !== tokenEmail) {
+                return res.status(403).send({ message: 'Forbidden Access!' });
+            }
+
+            const filter = { userEmail: email }
+            const allSubmissions = await submissionCollection.find(filter).toArray();
+
+            for (const submission of allSubmissions) {
+                const submissionId = submission.assignmentId;
+                const fullAssignmentData = await assignmentCollection.findOne(
+                    {
+                        _id: new ObjectId(submissionId)
+                    }
+                )
+                submission.title = fullAssignmentData.title;
+                submission.marks = fullAssignmentData.marks;
+            }
+
+            res.send(allSubmissions)
+        })
+
+
+        app.get('/pendings/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const tokenEmail = req.tokenEmail;
+
+            if (email !== tokenEmail) {
+                return res.status(403).send({ message: 'Forbidden Access!' });
+            }
+
+            const allPendings = await submissionCollection.find({
+                userEmail: { $ne: email },
+                status: { $ne: "Completed" }
+            }).toArray();
+
+            for (const pending of allPendings) {
+                const pendingId = pending.assignmentId;
+                const fullAssignmentData = await assignmentCollection.findOne({
+                    _id: new ObjectId(pendingId)
+                });
+                pending.title = fullAssignmentData?.title;
+                pending.marks = fullAssignmentData?.marks;
+            }
+
+            res.send(allPendings);
+        });
+
+
+        app.get('/assignments/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await assignmentCollection.findOne(query);
+            res.send(result);
+        })
+
+
+        app.put('/update-mark/:id', verifyJWT, async (req, res) => {
+            try {
+                const id = req.params.id;
+                const query = { _id: new ObjectId(id) };
+                const options = { upsert: true };
+
+                const updatedSubmission = req.body;
+                const updatedDoc = {
+                    $set: updatedSubmission
+                }
+
+                const result = await submissionCollection.updateOne(query, updatedDoc, options);
+                res.send(result);
+            }
+            catch (error) {
+                console.error(error);
+                res.status(500).json({ message: 'Update Failed', error: error.message });
+            }
+        })
+
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        //await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Welcome to OnlineStudyRoomDB Server')
+})
+
+app.listen(port, () => {
+    console.log(`server running at port ${port}`)
+})
